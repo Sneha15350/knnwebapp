d9f76e423b1c48a99ceffc0c8272049d3428922b
Sneha15350 <65900491+Sneha15350@users.noreply.github.com>
2021-10-12 21:08:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-#from sklearn.datasets import load_iris #loads the dataset
+from sklearn.datasets import load_iris #loads the dataset
 from sklearn.neighbors import KNeighborsClassifier
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pickle
+#import pickle
 st.title('IRIS CLASSIFIER USING KNN')
 st.write('Streamlit app for KNN iris dataset')
-#var=load_iris()
-with open('data.pkl', 'rb') as file:
-    var = pickle.load(file)
+var=load_iris()
+'''with open('data.pkl', 'rb') as file:
+    var = pickle.load(file)'''
 x=var.data #input
 y=var.target #output
 xmin=np.min(x,axis=0)
